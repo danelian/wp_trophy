5f6b3349c2e32b23960a7acfa1b032f500e9d408
danelian <141317269+danelian@users.noreply.github.com>
2023-11-22 07:34:05 +0300

    page 404

--- 404.php
+++ 404.php
@@ -3,8 +3,8 @@
 <section class="error-block">
   <div class="container">
     <span class="error-block__subtitle">404</span>
-    <h1 class="error-block__title">Такой страницы <br> не существует</h1>
-    <a href="/" class="button-second">На главную</a>
+    <h1 class="error-block__title"><?php echo __('This page', 'trophy'); ?><br><?php echo __('does not exist', 'trophy'); ?></h1>
+    <a href="<?php echo get_home_url(); ?>" class="button-second"><?php echo __('To main', 'trophy') ?></a>
   </div>
 </section>
 
